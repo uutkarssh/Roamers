e718379e56dcc84165e2abf6b9ecc66518cbec61
Utkarsh Maurya <utkarshmaurya88409@gmail.com>
2026-09-08 00:53:19 +0530

    Refresh service worker cache for solo mode

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE='roamers-shell-v2';
+const CACHE='roamers-shell-v3';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!=='roamers-shell-v2').map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!=='roamers-shell-v3').map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET')return;
   const url=new URL(e.request.url);
